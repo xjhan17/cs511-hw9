0352192e5c02ef0f1135a5abc2a0f167f861be9c
xjhan17 <71535733+xjhan17@users.noreply.github.com>
2020-11-13 21:32:31 -0500

    update p5 and p6

--- p5.py
+++ p5.py
@@ -5,73 +5,69 @@ import ast
 import json
 
 CPTs = [
-    ["V1", [[[], ["V1", "T"], 0.8], [[], ["V1", "F"], 0.2]]],
-    ["V2", [[[], ["V2", "T"], 0.7], [[], ["V2", "F"], 0.3]]],
-    ["V3", [[[["V1", "F"], ["V2", "F"]], ["V3", "F"], 0.9],
-           [[["V1", "F"], ["V2", "F"]], ["V3", "T"], 0.1],
-           [[["V1", "F"], ["V2", "T"]], ["V3", "F"], 0.2],
-           [[["V1", "F"], ["V2", "T"]], ["V3", "T"], 0.8],
-           [[["V1", "T"], ["V2", "F"]], ["V3", "F"], 0.3],
-           [[["V1", "T"], ["V2", "F"]], ["V3", "T"], 0.7],
-           [[["V1", "T"], ["V2", "T"]], ["V3", "F"], 0.4],
-           [[["V1", "T"], ["V2", "T"]], ["V3", "T"], 0.6]]],
-    ["V4", [[[], ["V4", "T"], 0.1], [[], ["V4", "F"], 0.9]]],
-    ["V5", [[[["V3", "F"], ["V4", "F"]], ["V5", "F"], 0.1],
-           [[["V3", "F"], ["V4", "F"]], ["V5", "T"], 0.9],
-           [[["V3", "F"], ["V4", "T"]], ["V5", "F"], 0.2],
-           [[["V3", "F"], ["V4", "T"]], ["V5", "T"], 0.8],
-           [[["V3", "T"], ["V4", "F"]], ["V5", "F"], 0.7],
-           [[["V3", "T"], ["V4", "F"]], ["V5", "T"], 0.3],
-           [[["V3", "T"], ["V4", "T"]], ["V5", "F"], 0.4],
-           [[["V3", "T"], ["V4", "T"]], ["V5", "T"], 0.6]]]
+    ["V1", [[[], ["V1", "T"], 0.3], [[], ["V1", "F"], 0.7]]],
+    ["V2", [[[], ["V2", "T"], 0.2], [[], ["V2", "F"], 0.8]]],
+    ["V3", [[[], ["V3", "T"], 0.9], [[], ["V3", "F"], 0.1]]],
+    ["V4", [[[["V1", "T"], ["V2", "T"]], ["V4", "T"], 0.2],
+           [[["V1", "T"], ["V2", "T"]], ["V4", "F"], 0.8],
+           [[["V1", "T"], ["V2", "F"]], ["V4", "T"], 0.9],
+           [[["V1", "T"], ["V2", "F"]], ["V4", "F"], 0.1],
+           [[["V1", "F"], ["V2", "T"]], ["V4", "T"], 0.1],
+           [[["V1", "F"], ["V2", "T"]], ["V4", "F"], 0.9],
+           [[["V1", "F"], ["V2", "F"]], ["V4", "T"], 0.4],
+           [[["V1", "F"], ["V2", "F"]], ["V4", "F"], 0.4]]],
+    ["V5", [[[["V2", "T"], ["V2", "T"]], ["V5", "T"], 0.5],
+           [[["V2", "T"], ["V3", "T"]], ["V5", "F"], 0.5],
+           [[["V2", "T"], ["V3", "F"]], ["V5", "T"], 0.6],
+           [[["V2", "T"], ["V3", "F"]], ["V5", "F"], 0.4],
+           [[["V2", "F"], ["V3", "T"]], ["V5", "T"], 0.3],
+           [[["V2", "F"], ["V3", "T"]], ["V5", "F"], 0.7],
+           [[["V2", "F"], ["V3", "F"]], ["V5", "T"], 0.1],
+           [[["V2", "F"], ["V3", "F"]], ["V5", "F"], 0.9]]],
+    ["V6", [[[["V5", "F"]], ["V6", "F"], 0.1],
+           [[["V5", "F"]], ["V6", "T"], 0.9],
+           [[["V5", "F"]], ["V6", "F"], 0.5],
+           [[["V5", "F"]], ["V6", "T"], 0.5]]],
+    ["V7", [[[["V4", "F"]], ["V7", "F"], 0.7],
+           [[["V4", "F"]], ["V7", "T"], 0.3],
+           [[["V4", "F"]], ["V7", "F"], 0.4],
+           [[["V4", "F"]], ["V7", "T"], 0.6]]],     
+    ["V8", [[[["V4", "T"], ["V5", "T"]], ["V8", "T"], 0.4],
+           [[["V4", "T"], ["V5", "T"]], ["V8", "F"], 0.6],
+           [[["V4", "T"], ["V5", "F"]], ["V8", "T"], 0.5],
+           [[["V4", "T"], ["V5", "F"]], ["V8", "F"], 0.5],
+           [[["V4", "F"], ["V5", "T"]], ["V8", "T"], 0.6],
+           [[["V4", "F"], ["V5", "T"]], ["V8", "F"], 0.4],
+           [[["V4", "F"], ["V5", "F"]], ["V8", "T"], 0.7],
+           [[["V4", "F"], ["V5", "F"]], ["V8", "F"], 0.3]]]
+
 ]
 Os = [["V1", "T"]]
-
 
 def main():
     listofResults = MPE(CPTs, Os)
     print(listofResults)
 
-def objectiveFunctions(CPTs, Os, variablesZ3, var_dependencies):
-    objectiveFunction = 1
-    for cpt in CPTs:
-        var = cpt[0]
-        indepProbability = 0
-        conditionalProbability = 0
-        if len(var_dependencies[var]) == 0:
-            for triplets in cpt[1]:
-                indepProbability  += (triplets[2] * 100 * variablesZ3[var]) if triplets[1][1] == "T" else (triplets[2] * 100 * (1-variablesZ3[var]))                    
-            objectiveFunction *= indepProbability
-        else:
-            for triplets in cpt[1]:
-                prob = 1
-                for cond in triplets[0]:
-                    prob *= variablesZ3[cond[0]] if cond[1] == "T" else (1-variablesZ3[cond[0]])
-                prob *= (triplets[2] * 100) * variablesZ3[var] if triplets[1][1] == "T" else (triplets[2] * 100) * (1-variablesZ3[var])
-                conditionalProbability += prob
-            objectiveFunction *= conditionalProbability
-    return objectiveFunction
-
 # function MPE(CPTs, Os) returns a list of pairs of variables and values
 def MPE(CPTs, Os):
     variablesZ3 = {}
-    var_dependencies = {}
+    triplets = {}
     s = Optimize()
 
     for cpt in CPTs:
-        var_dependencies[cpt[0]] = []
-        triplets = cpt[1][0]
-        for dependent in triplets[0]:
-            var_dependencies[cpt[0]].append(dependent[0])
+        triplets[cpt[0]] = []
+        triplet = cpt[1][0]
+        for dependent in triplet[0]:
+            triplets[cpt[0]].append(dependent[0])
 
     for observed_pair in Os:
         variablesZ3[observed_pair[0]] = 1 if observed_pair[1] == "T" else 0
-    for var in var_dependencies:
+    for var in triplets:
         if var not in variablesZ3.keys():
             variablesZ3[var] = Int(var)
             s.add(And(Int(var) >= 0, Int(var) <= 1))
 
-    objectiveFunction = objectiveFunctions(CPTs, Os, variablesZ3, var_dependencies)
+    objectiveFunction = objectiveFunctions(CPTs, Os, variablesZ3, triplets)
     s.maximize(objectiveFunction)
     if s.check() == sat:
         listofResults = []
@@ -81,6 +77,28 @@ def MPE(CPTs, Os):
         return listofResults
     else:
         print("False: error to solve the problem!")
+
+
+def objectiveFunctions(CPTs, Os, variablesZ3, triplets):
+    objectiveFunction = 1
+    for cpt in CPTs:
+        var = cpt[0]
+        indepProbability = 0
+        conditionalProbability = 0
+        if len(triplets[var]) == 0:
+            for triplet in cpt[1]:
+                indepProbability  += (triplet[2] * 100 * variablesZ3[var]) if triplet[1][1] == "T" else (triplet[2] * 100 * (1-variablesZ3[var]))                    
+            objectiveFunction *= indepProbability
+        else:
+            for triplet in cpt[1]:
+                prob = 1
+                for cond in triplet[0]:
+                    prob *= variablesZ3[cond[0]] if cond[1] == "T" else (1-variablesZ3[cond[0]])
+                prob *= (triplet[2] * 100) * variablesZ3[var] if triplet[1][1] == "T" else (triplet[2] * 100) * (1-variablesZ3[var])
+                conditionalProbability += prob
+            objectiveFunction *= conditionalProbability
+    return objectiveFunction
+
 
 
 if __name__== "__main__":
--- p6.py
+++ p6.py
@@ -5,30 +5,47 @@ import ast
 import json
 
 CPTs = [
-    ["V1", [[[], ["V1", "T"], 0.8], [[], ["V1", "F"], 0.2]]],
-    ["V2", [[[], ["V2", "T"], 0.7], [[], ["V2", "F"], 0.3]]],
-    ["V3", [[[["V1", "F"], ["V2", "F"]], ["V3", "F"], 0.9],
-           [[["V1", "F"], ["V2", "F"]], ["V3", "T"], 0.1],
-           [[["V1", "F"], ["V2", "T"]], ["V3", "F"], 0.2],
-           [[["V1", "F"], ["V2", "T"]], ["V3", "T"], 0.8],
-           [[["V1", "T"], ["V2", "F"]], ["V3", "F"], 0.3],
-           [[["V1", "T"], ["V2", "F"]], ["V3", "T"], 0.7],
-           [[["V1", "T"], ["V2", "T"]], ["V3", "F"], 0.4],
-           [[["V1", "T"], ["V2", "T"]], ["V3", "T"], 0.6]]],
-    ["V4", [[[], ["V4", "T"], 0.1], [[], ["V4", "F"], 0.9]]],
-    ["V5", [[[["V3", "F"], ["V4", "F"]], ["V5", "F"], 0.1],
-           [[["V3", "F"], ["V4", "F"]], ["V5", "T"], 0.9],
-           [[["V3", "F"], ["V4", "T"]], ["V5", "F"], 0.2],
-           [[["V3", "F"], ["V4", "T"]], ["V5", "T"], 0.8],
-           [[["V3", "T"], ["V4", "F"]], ["V5", "F"], 0.7],
-           [[["V3", "T"], ["V4", "F"]], ["V5", "T"], 0.3],
-           [[["V3", "T"], ["V4", "T"]], ["V5", "F"], 0.4],
-           [[["V3", "T"], ["V4", "T"]], ["V5", "T"], 0.6]]]
+    ["V1", [[[], ["V1", "T"], 0.3], [[], ["V1", "F"], 0.7]]],
+    ["V2", [[[], ["V2", "T"], 0.2], [[], ["V2", "F"], 0.8]]],
+    ["V3", [[[], ["V3", "T"], 0.9], [[], ["V3", "F"], 0.1]]],
+    ["V4", [[[["V1", "T"], ["V2", "T"]], ["V4", "T"], 0.2],
+           [[["V1", "T"], ["V2", "T"]], ["V4", "F"], 0.8],
+           [[["V1", "T"], ["V2", "F"]], ["V4", "T"], 0.9],
+           [[["V1", "T"], ["V2", "F"]], ["V4", "F"], 0.1],
+           [[["V1", "F"], ["V2", "T"]], ["V4", "T"], 0.1],
+           [[["V1", "F"], ["V2", "T"]], ["V4", "F"], 0.9],
+           [[["V1", "F"], ["V2", "F"]], ["V4", "T"], 0.4],
+           [[["V1", "F"], ["V2", "F"]], ["V4", "F"], 0.4]]],
+    ["V5", [[[["V2", "T"], ["V2", "T"]], ["V5", "T"], 0.5],
+           [[["V2", "T"], ["V3", "T"]], ["V5", "F"], 0.5],
+           [[["V2", "T"], ["V3", "F"]], ["V5", "T"], 0.6],
+           [[["V2", "T"], ["V3", "F"]], ["V5", "F"], 0.4],
+           [[["V2", "F"], ["V3", "T"]], ["V5", "T"], 0.3],
+           [[["V2", "F"], ["V3", "T"]], ["V5", "F"], 0.7],
+           [[["V2", "F"], ["V3", "F"]], ["V5", "T"], 0.1],
+           [[["V2", "F"], ["V3", "F"]], ["V5", "F"], 0.9]]],
+    ["V6", [[[["V5", "F"]], ["V6", "F"], 0.1],
+           [[["V5", "F"]], ["V6", "T"], 0.9],
+           [[["V5", "F"]], ["V6", "F"], 0.5],
+           [[["V5", "F"]], ["V6", "T"], 0.5]]],
+    ["V7", [[[["V4", "F"]], ["V7", "F"], 0.7],
+           [[["V4", "F"]], ["V7", "T"], 0.3],
+           [[["V4", "F"]], ["V7", "F"], 0.4],
+           [[["V4", "F"]], ["V7", "T"], 0.6]]],     
+    ["V8", [[[["V4", "T"], ["V5", "T"]], ["V8", "T"], 0.4],
+           [[["V4", "T"], ["V5", "T"]], ["V8", "F"], 0.6],
+           [[["V4", "T"], ["V5", "F"]], ["V8", "T"], 0.5],
+           [[["V4", "T"], ["V5", "F"]], ["V8", "F"], 0.5],
+           [[["V4", "F"], ["V5", "T"]], ["V8", "T"], 0.6],
+           [[["V4", "F"], ["V5", "T"]], ["V8", "F"], 0.4],
+           [[["V4", "F"], ["V5", "F"]], ["V8", "T"], 0.7],
+           [[["V4", "F"], ["V5", "F"]], ["V8", "F"], 0.3]]]
+
 ]
 Os = [["V1", "T"]]
-Vs = ["V5"]
+Vs = ["V8"]
 
-dependencies = {}
+triplets = {}
 variablesZ3Py = {}
 
 
@@ -40,7 +57,7 @@ def main():
 def MAP(CPTs, Os, Vs):
     s = Optimize()
     for cpt in CPTs:
-        dependencies[cpt[0]] = cpt[1]
+        triplets[cpt[0]] = cpt[1]
 
     observed_vars = []
     for pairs in Os:
@@ -66,24 +83,23 @@ def objectiveFunctions(CPTs, Os, Vs):
     objectiveFunction = 1
     for variables in Vs:
         variablesZ3Py[variables] = Int(variables)
-        [true, false] = recursiveFunction(variables)
-        objectiveFunction *= (Int(variables)*true+(1-Int(variables))*false)
+        trueProbability, falseProbabilty = dfs(variables)
+        objectiveFunction *= (Int(variables)*trueProbability+(1-Int(variables))*falseProbabilty)
     return objectiveFunction
-    
-def recursiveFunction(variables):
-    triplets = dependencies[variables]
+
+def dfs(variables):
     trueProbability = 0
     falseProbabilty = 0
-    for triplet in triplets:
+    for triplet in triplets[variables]:
         if len(triplet[0]) == 0:
             if triplet[1][1] == "T":
                 trueProbability = triplet[2]*100
             else:
-                falseProbabilty = triplet[2]* 00
+                falseProbabilty = triplet[2]*100
         else:
             variableProbability = triplet[2]*100
             for condition in triplet[0]:
-                [trueDep, falseDep] = recursiveFunction(condition[0])
+                [trueDep, falseDep] = dfs(condition[0])
                 if condition[0] not in variablesZ3Py.keys():
                     variableProbability *= trueDep if condition[1] == "T" else falseDep
                 else:
@@ -92,7 +108,7 @@ def recursiveFunction(variables):
                 trueProbability += variableProbability
             else:
                 falseProbabilty += variableProbability
-    return [trueProbability, falseProbabilty]
+    return trueProbability, falseProbabilty
 
 
 if __name__== "__main__":
